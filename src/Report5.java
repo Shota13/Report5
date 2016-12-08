f2fb01670cb7a4c74944ae52d01d861d5bb29334
Shota Nagamine <e165757@ie.u-ryukyu.ac.jp>
2016-12-08 16:56:09 +0900

    Report5
    step3

--- src/Report5.java
+++ src/Report5.java
@@ -3,8 +3,10 @@
  */
 public class Report5 {
     public static void main(String args[]) throws NullPointerException {
-           try{ String str = null;
+        try{ String str = "3．14";
+            double value;
             str.length();
+            value=Double.parseDouble(str);
         }catch (NullPointerException e){
                System.out.println("”NullPointerException”が発生");
                e.printStackTrace();
