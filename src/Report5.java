2c18b802bb897a30ad78f7e3aedd425a99ee23f9
Shota Nagamine <e165757@ie.u-ryukyu.ac.jp>
2016-12-08 16:45:57 +0900

    Report5

--- src/Report5.java
+++ src/Report5.java
@@ -2,9 +2,13 @@
  * Created by e165757 on 2016/12/02.
  */
 public class Report5 {
-    public static void main(String args[]){
-    String str = null;
-    str.length();
+    public static void main(String args[]) throws NullPointerException {
+           try{ String str = null;
+            str.length();
+        }catch (NullPointerException e){
+               System.out.println("”NullPointerException”が発生");
+               e.printStackTrace();
+           }
 
     }
 }
